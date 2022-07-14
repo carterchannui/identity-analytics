80cbef135d9dffd51d9d2625aee88d08310d7431
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-07-13 17:58:14 -0700

    formmated code

--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -29,35 +29,40 @@ import Sidebar from "../../components/Sidebar/sidebar"
 const home = () => {
   return (
         <Flex h='100%' w="100%" bg='#E7E7E7' p='4' m='8' borderRadius="10px">
-            <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
-                <Stat>
-                    <StatLabel> Passes Issued </StatLabel>
-                    <StatNumber> 40,000 </StatNumber>
-                </Stat>
+            <Box w='100%'>
+                <Flex>
+                    <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
+                        <Stat>
+                            <StatLabel> Passes Issued </StatLabel>
+                            <StatNumber> 40,000 </StatNumber>
+                        </Stat>
+                    </Box>
+                    <Spacer/>
+                    <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
+                        <Stat>
+                            <StatLabel> Passes Issued </StatLabel>
+                            <StatNumber> 40,000 </StatNumber>
+                        </Stat>
+                    </Box>
+                    <Spacer/>
+                    <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
+                        <Stat>
+                            <StatLabel> Passes Issued </StatLabel>
+                            <StatNumber> 40,000 </StatNumber>
+                        </Stat>
+                    </Box>
+                    <Spacer/>
+                    <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
+                        <Stat>
+                            <StatLabel> Passes Issued </StatLabel>
+                            <StatNumber> 40,000 </StatNumber>
+                        </Stat>
+                    </Box>
+                </Flex>
+                <Box p='8'>
+                    <Chart/>
+                </Box>
             </Box>
-            <Spacer/>
-            <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
-                <Stat>
-                    <StatLabel> Passes Issued </StatLabel>
-                    <StatNumber> 40,000 </StatNumber>
-                </Stat>
-            </Box>
-            <Spacer/>
-            <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
-                <Stat>
-                    <StatLabel> Passes Issued </StatLabel>
-                    <StatNumber> 40,000 </StatNumber>
-                </Stat>
-            </Box>
-            <Spacer/>
-            <Box bg='#FFFFFF' px='8' py='4' borderRadius="10px" boxShadow="-1px 7px 5px 1px rgba(0,0,0,0.30);">
-                <Stat>
-                    <StatLabel> Passes Issued </StatLabel>
-                    <StatNumber> 40,000 </StatNumber>
-                </Stat>
-            </Box>
-
-
     </Flex>
   )
 }
